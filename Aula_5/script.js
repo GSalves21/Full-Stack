fa400e9e888c4410e5a1bab45781bf3e225a7536
Gabriel <ga4718962@gmail.com>
2026-03-11 17:07:10 -0300

    aula 5

---
+++ Aula_5/script.js
@@ -0,0 +1,29 @@
+// equivalente ao print do pyton
+console.log("Olá classe")
+console.log("Exemplo")
+
+//  let nome=prompt("Digite o seu nome:") 
+//  pedir informação do usuario
+// console.log(nome) 
+// mandar infromação do usuario
+// window.alert("Seu nome é: " + nome)
+
+// 
+// console.log("exercicio 1")
+// for(i=1; i<=100; i=i+1)
+//     console.log(i)
+
+// console.log("exercicio 2")
+// let i = 5;
+// while(i<=500){
+//     console.log(i);
+//     i= i + 5;
+// 
+
+console.log("Exercicio 3")
+let numero = prompt("digite um numero:")
+let b = numero;
+while(b>0){
+    b=b-1
+    console.log(b)
+}
