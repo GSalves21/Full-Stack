4092650d5d830c3fb79a6025898167e0fb0ec60a
GSalves21 <ga4718962@gmail.com>
2026-05-19 18:13:32 -0300

    fiz um servidor especifico para rodas os labs

---
+++ todos_labs/server.js
@@ -0,0 +1,21 @@
+// primera vez fazendo o servidor sozinho
+
+// chama a biblioteca http
+var http = require('http');
+
+//chama a biblioteca express
+var express = require('express');
+
+var app = express();
+
+//quando o servidor rodar redirecionará para a pagina html que estiver dentro dessa pasta
+app.use(express.static('./public'));
+
+//cria o servidor e entrega as requisições pro express as fazer
+var servidor = http.createServer(app);
+
+//porta do servidor
+servidor.listen(80);
+
+
+console.log('funcionou');
