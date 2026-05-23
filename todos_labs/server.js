a7ecb621f97450cf06fe63b2d921b961fa2ac0eb
GSalves21 <ga4718962@gmail.com>
2026-05-23 16:00:42 -0300

    fiz o lab_8

--- todos_labs/server.js
+++ todos_labs/server.js
@@ -8,18 +8,50 @@ var express = require('express');
 
 var app = express();
 
-//quando o servidor rodar redirecionará para a pagina html que estiver dentro dessa pasta
-app.use(express.static('./public'));
+let bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
-app.get('/',function(req,resp){
-    resp.redirect('projects.html')
-})
+app.set('view engine','ejs');
+app.set('views','./views');
 
 //cria o servidor e entrega as requisições pro express as fazer
 var servidor = http.createServer(app);
 
 //porta do servidor
 servidor.listen(80);
+
+//quando o servidor rodar redirecionará para a pagina html que estiver dentro dessa pasta
+app.use(express.static('./public'));
+
+
+app.get('/',function(req,resp){
+    resp.redirect('projects.html');
+})
+
+app.get('/cadastra',function(req,resp){
+    resp.redirect('/LAB/LAB_8/Cadastro.html');
+})
+
+app.get('/login',function(req,resp){
+    resp.redirect('/LAB/LAB_8/Login.html');
+})
+
+app.post('/validar_cadastro',function(req,resp){
+    var usuario = req.body.usuario;
+    var email = req.body.email;
+    var senha = req.body.senha;
+    resp.render('cadastro_status', {usuario,email,senha});
+})
+
+app.post('/logar',function(req,resp){
+    var usuario = req.body.usuario;
+    var email = req.body.email;
+    var senha = req.body.senha;
+    resp.render('resposta', {usuario,email,senha})
+})
+
+
 
 
 console.log('funcionou');
