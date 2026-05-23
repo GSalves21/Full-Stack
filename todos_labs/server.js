b03e2f2eb0800e66c8cf375144043d75b47461d1
GSalves21 <ga4718962@gmail.com>
2026-05-23 11:29:17 -0300

    coloquei pra requisição / levar pra projects html,atualizei os links

--- todos_labs/server.js
+++ todos_labs/server.js
@@ -11,6 +11,10 @@ var app = express();
 //quando o servidor rodar redirecionará para a pagina html que estiver dentro dessa pasta
 app.use(express.static('./public'));
 
+app.get('/',function(req,resp){
+    resp.redirect('projects.html')
+})
+
 //cria o servidor e entrega as requisições pro express as fazer
 var servidor = http.createServer(app);
 
